8c1bc1260ca5cb5f4c4b5a0049522ae2a3755509
vikas6220 <vikas.srivastava94@gmail.com>
2026-04-08 22:00:23 +0530

    Update 01_gemini_chatbot.py

--- 01_gemini_chatbot.py
+++ 01_gemini_chatbot.py
@@ -3,6 +3,7 @@ import os
 import google.generativeai as genai
 from dotenv import load_dotenv
 
+## here you need to provide secret keys for streamlit deployment
 st.secrets["GEMINI_API_KEY"]
 
 # ---------------- Setup ---------------- #
